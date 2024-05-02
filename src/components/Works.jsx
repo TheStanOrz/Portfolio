815a52e1b4f7bc8ae101d83447fc983072a0390b
TheStanOrz <hhh900511@gmail.com>
2024-05-02 15:57:21 +0800

    fix: layout

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -67,7 +67,7 @@ const Works = () => {
           links to code repositories & live demos in it.
         </motion.p>
       </div>
-      <div className="mt-20 flex flex-wrap gap-7">
+      <div className="mt-10 flex flex-wrap gap-7">
         {projects.map((project,index)=>(
           <ProjectCard 
             key={`project-${index}`}
