60a421d859b8483d67c51729cefbcaf4fc52f005
TheStanOrz <hhh900511@gmail.com>
2024-05-06 11:04:58 +0800

    docs: mobile menu uncommented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        {/* <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer'
           onClick={()=>setToggle(!toggle)} />
           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
@@ -57,7 +57,7 @@ const Navbar = () => {
           ))}
         </ul>
           </div>
-        </div> */}
+        </div>
       </div>
     </nav>
   )
